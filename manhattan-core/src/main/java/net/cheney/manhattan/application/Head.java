ee2bcff3b90fbba900c84f2d6d8fc30ef0623cbb
Dave Cheney <dave@cheney.net>
2010-07-02 20:00:33 +1000

    Tight arse HEAD support

--- manhattan-core/src/main/java/net/cheney/manhattan/application/Head.java
+++ manhattan-core/src/main/java/net/cheney/manhattan/application/Head.java
@@ -4,11 +4,9 @@ import net.cheney.cocktail.application.Environment;
 import net.cheney.cocktail.message.Header;
 import net.cheney.cocktail.message.Response;
 import net.cheney.cocktail.message.Response.Builder;
-import net.cheney.cocktail.message.Response.Status;
-import net.cheney.manhattan.resource.Resource;
 import net.cheney.manhattan.resource.ResourceProvidor;
 
-public class Head extends BaseApplication {
+public class Head extends Get {
 
 	public Head(ResourceProvidor providor) {
 		super(providor);
@@ -16,12 +14,15 @@ public class Head extends BaseApplication {
 
 	@Override
 	public Response call(Environment env) {
-		Resource resource = resolveResource(env);
-		resource.activeLocks();
-		Builder builder = Response.builder(Status.SUCCESS_OK);
-		builder.header(Header.LAST_MODIFIED).add(RFC1123_DATE_FORMAT.format(resource.lastModified()));
-		builder.header(Header.CONTENT_LENGTH).add(Long.toString(resource.contentLength()));
-		return builder.build();
+		Response get = super.call(env);
+		Builder head = Response.builder(get.status());
+		for(Header key : get.keys()) {
+			head.header(key).set(get.header(key));
+		}
+		if(get.hasBody()) {
+			head.header(Header.CONTENT_LENGTH).add(Long.toString(get.body().remaining()));
+		}
+		return head.build();
 	}
 
 }
